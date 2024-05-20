c2a4da4bd0e8ce672591e633b8346147eae40a57
Amanda Mark <amarkpark@gmail.com>
2024-05-20 14:26:04 -0600

    First edits, have added about page and links

---
+++ app/about/page.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+import Link from "next/link"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Link href="/" class="text-3xl">Home</Link>
+      <h1 class="text-6xl">AboutPage</h1>
+    </div>
+  )
+}
+
+export default AboutPage
