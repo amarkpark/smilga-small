736343078320c6fc264f0631359a3cf48d71941e
Amanda Mark <amarkpark@gmail.com>
2024-05-20 15:31:37 -0600

    Add nested route info page

---
+++ app/about/info/page.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+import Link from "next/link"
+
+const InfoPage = () => {
+  return (
+    <div>
+      <Link href="/" class="text-3xl">Home</Link>
+      <hr />
+      <h1 class="text-6xl">InfoPage</h1>
+    </div>
+  )
+}
+
+export default InfoPage
--- app/about/page.js
+++ app/about/page.js
@@ -6,7 +6,11 @@ const AboutPage = () => {
   return (
     <div>
       <Link href="/" class="text-3xl">Home</Link>
+      <hr />
       <h1 class="text-6xl">AboutPage</h1>
+      <div class="px-10 py-10">
+        <Link href="/about/info" class="text-3xl">Info</Link>
+      </div>
     </div>
   )
 }
