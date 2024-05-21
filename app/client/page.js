f01f09f1c3f32961d6ffacbaeb7b7d2820062b9e
Amanda Mark <amarkpark@gmail.com>
2024-05-21 14:26:05 -0600

    Lesson 18 - Fetch Data, also cleanup

--- app/client/page.js
+++ app/client/page.js
@@ -1,12 +1,7 @@
 "use client"
-// import { useState } from "react/cjs/react.production.min";
-// import { useState } from "react";
 import Counter from "@/components/Counter";
 
-
-
 const ClientPage = () => {
-  // const [count, setCount] = useState(0);
   return (
     <div>
       <h1 className="text-6xl mb-4">Client Page</h1>
--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,7 +1,12 @@
-const DrinksPage = () => {
+const drinksUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=s";
+
+const DrinksPage = async () => {
+  const response = await fetch(drinksUrl);
+  const data = await response.json();
+  console.log(data.drinks);
   return (
     <div>
-      <h1 className="text-6xl">DrinksPage</h1>
+      <h1 className="text-6xl">Drinks</h1>
       <hr />
     </div>
   )
