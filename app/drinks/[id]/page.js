16d9918b32001ff5040b4858fca7aaeacfa6198d
Amanda Mark <amarkpark@gmail.com>
2024-05-23 17:37:17 -0600

    Slight tweaks

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.phpp?i=';
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 const getDrinkDetail = async (drinkUrl) => {
   const response = await fetch(drinkUrl);
   if (!response.ok) {
@@ -21,13 +21,15 @@ const drinkDetail = async ({params}) => {
   console.log(params.id);
   return (
     <div className="flex flex-col max-w-xl mx-auto items-center">
-      <div className="card card-compact justify-self-center w-96 bg-base-100 shadow-xl">
+      <h1 className="text-3xl mb-4">Your beverage is served...</h1>
+      <div className="card card-compact justify-self-center w-96 bg-base-100 shadow-xl mb-16">
         <div className="card-body">
           <h2 className="card-title">{drinkObject.strDrink}</h2>
           <p>{drinkObject.strInstructions}</p>
         </div>
         <figure><img src={drinkObject.strDrinkThumb} alt={drinkObject.strDrink} /></figure>
       </div>
+      <Link href="/drinks" className="btn btn-accent">Return to Drinks</Link>
     </div>
   );
 }
--- app/drinks/layout.js
+++ app/drinks/layout.js
@@ -1,9 +1,7 @@
 const DrinksLayout = ({children}) => {
   return (
     <div className="flex flex-col flex-grow max-w-4xl mx-auto items-center pb-8">
-      {/* <div className="grid space-around justify-self-center w-96  h-20 card bg-base-300 rounded-box place-items-center">
-        Your cocktail selection:
-      </div> */}
+      
       <div className="flex flex-col w-full border-opacity-50">
         {children}
       </div>
