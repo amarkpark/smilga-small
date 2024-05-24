3739192e7812f7b59b72f5081a6d383c9c480d19
Amanda Mark <amarkpark@gmail.com>
2024-05-24 15:15:59 -0600

    Lesson 28 - More routing
    Square brackets present problems with VSCode and/or Mac and/or a combination.

---
+++ app/(dashboard)/auth/[[...sign-in]]/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SignInPage = ({params}) => {
+  console.log(params);
+  return (
+    <h1 className="text-6xl">SignInPage</h1>
+  );
+};
+
+export default SignInPage
