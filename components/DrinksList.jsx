351605ceae9056b834fb5b837f4adf8ca33a7821
Amanda Mark <amarkpark@gmail.com>
2024-05-24 13:56:10 -0600

    Lesson 29 - Responsive Images
    I added the code from the course as a reminder:
    
    https://github.com/john-smilga/nextjs-course-openai/blob/main/01-nextjs-tutorial/components/DrinksList.jsx
    
    But the DaisyUI cards are already more responsive than the menu.

--- components/DrinksList.jsx
+++ components/DrinksList.jsx
@@ -22,7 +22,34 @@ const DrinksList = ({drinks}) => {
         )
       })}
     </ul>
-  )
+  );
+
+  // Suggested by the course - meh
+  // return (
+  //   <ul className='grid sm:grid-cols-2 gap-6 mt-6'>
+  //     {drinks.map((drink) => {
+  //       return (
+  //         <li key={drink.idDrink}>
+  //           <Link
+  //             href={`/drinks/${drink.idDrink}`}
+  //             className='text-xl font-medium'
+  //           >
+  //             <div className='relative h-48 mb-4'>
+  //               <Image
+  //                 src={drink.strDrinkThumb}
+  //                 fill
+  //                 sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw'
+  //                 alt={drink.strDrink}
+  //                 className='rounded-md object-cover'
+  //               />
+  //             </div>
+  //             {drink.strDrink}
+  //           </Link>
+  //         </li>
+  //       );
+  //     })}
+  //   </ul>
+  // );
 };
 
 export default DrinksList
