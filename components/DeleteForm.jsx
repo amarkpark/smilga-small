0858cc6dad5ac0ccf3229f68b1053c9b820a0883
Amanda Mark <amarkpark@gmail.com>
2024-07-05 16:01:21 -0600

    Lesson 43 - Add pending status to delete button

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,16 +1,28 @@
+"use client"
 import { deleteTask } from "@/utils/actions";
+import { useFormStatus } from "react-dom";
+
+const SubmitButton = ({id}) => {
+  const {pending} = useFormStatus();
+
+  console.log("pending", pending);
+  return (
+    <button
+      id={`deleteTask-${id}`}
+      name={`deleteTask-${id}`}
+      className="btn btn-link"
+      disabled={pending}
+    >
+      {pending ? "Pending..." : "Delete"}
+    </button>
+  )
+}
 
 const DeleteForm = ({id}) => {
   return (
     <form action={deleteTask}>
       <input type="hidden" name="id" value={id} />
-      <button
-        id={`deleteTask-${id}`}
-        name={`deleteTask-${id}`}
-        className="btn btn-link"
-      >
-        Delete
-      </button>
+      <SubmitButton {...{id}}/>
     </form>
   )
 }
