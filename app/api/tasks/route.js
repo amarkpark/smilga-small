ea2eb0fb1a5461f7ebc6f0799fdbb05a2adc5233
Amanda Mark <amarkpark@gmail.com>
2024-07-06 14:30:28 -0600

    Lesson 46 Implicit Route Handler: POST

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -1,6 +1,19 @@
 import db from "@/utils/db";
+import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
   const tasks = await db.task.findMany();
-  return Response.json({ data: tasks, status: 200 });
-};
+  return NextResponse.json({ data: tasks }, { status: 200 });
+}
+
+export const POST = async (request) => {
+  const data = await request.json();
+  const tasks = await db.task.create(
+    { data:
+      {
+        content: data.taskContent
+      }
+    }
+  );
+  return NextResponse.json({ data: tasks }, { status: 200 });
+}
